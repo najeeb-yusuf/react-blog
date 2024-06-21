cace1674015e49340adc1c20fdce3598820618bc
najeeb-yusuf <yusufnajlawal@gmail.com>
2024-06-21 18:23:12 +0100

    Making articles dynamic

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainEntry from './MainEntry';
+import articlesData from './data/articles.json'; 
+
+
+
 
 export default function Main() {
-    return(
+    const articles = articlesData;
+    return (
         <main>
             <div className="container">
-                <h4 className='post-type | fw-heading text-primary'> Latest</h4>
-                <MainEntry />
-                <MainEntry />
-                <MainEntry />
-                <MainEntry />
-                <MainEntry />
-                <MainEntry />
+                <h4 className='post-type | fw-heading text-primary'>Latest</h4>
+                {articles.map((article, index) => (
+                    <MainEntry key={index} params={article} />
+                ))}
             </div>
         </main>
-    )
+    );
 }
--- src/components/MainEntry.js
+++ src/components/MainEntry.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export default function MainEntry(params) {
+export default function MainEntry({ params }) {
+    const { title, content, tags, date, username } = params;
+
+    // Format the date
+    const formattedDate = new Date(date).toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: 'short'
+    });
+
     return (
         <section className='hidden'>
             <div className="container">
@@ -8,30 +16,26 @@ export default function MainEntry(params) {
                     <div className="columns">
                         <div className='article'>
                             <h2 className="text-primary-400 fw-4 fs-heading ff-heading">
-                                15 Disadvantages Of Freedom And How You Can Workaround It.
+                                {title}
                             </h2>
                             <p className="fw-2 fs-body text-neutral-100 ff-body">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum <a href="https://google.com" className="read-more | text-primary-400">...read more</a>
+                                {content}<a href="https://www.najeebyusuf.com" className="read-more | text-primary-400">...read more</a>
                             </p>
                             <div className="date-user">
-                                <p className="date | fw-6 text-neutral-100"> 27 May</p>
-                                <p className="user | fw-3 text-neutral-100"> @notnajeeb</p>
+                                <p className="date | fw-6 text-neutral-100">{formattedDate}</p>
+                                <p className="user | fw-3 text-neutral-100">@{username}</p>
                             </div>
                             <div className='tags'>
-                                <button className="button btn-inverse text-primary-400 fw-500">
-                                    #meditation
-                                </button>
-                                <button className="button btn-inverse text-primary-400 fw-500">
-                                    #mentalpeace
-                                </button>
+                                {tags && tags.map((value, index) => (
+                                    <button className="button btn-inverse text-primary-400 fw-500" key={index}>
+                                        {value}
+                                    </button>
+                                ))}
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-
         </section>
-    )
+    );
 }
-
